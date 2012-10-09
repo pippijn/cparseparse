041cb3229d0a45440ea28498947a407cd09d350e
Pippijn van Steenhoven <pip88nl@gmail.com>
2012-10-09 13:47:46 +0800

    add Timing.progress for overall timing

--- elkhound/baselib/timing.ml
+++ elkhound/baselib/timing.ml
@@ -1,10 +1,30 @@
+let trace_progress = true
+
+let start = Unix.gettimeofday ()
+
+
+let progress desc f x =
+  let result = f x in
+  let finish = Unix.gettimeofday () in
+
+  if trace_progress then (
+    print_string "%%% ";
+    Printf.printf "[%fs] %s\n" (finish -. start) desc;
+    flush stdout;
+  );
+
+  result
+
+
 let time desc f x =
   let start = Unix.gettimeofday () in
   let result = f x in
   let finish = Unix.gettimeofday () in
 
-  print_string "%%% ";
-  Printf.printf "%s took %f seconds\n" desc (finish -. start);
-  flush stdout;
+  if trace_progress then (
+    print_string "%%% ";
+    Printf.printf "%s took %fs\n" desc (finish -. start);
+    flush stdout;
+  );
 
   result
