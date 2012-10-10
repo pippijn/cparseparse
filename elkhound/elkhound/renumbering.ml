595e159d21c8c58471327b0223cb267476c1a0d7
Pippijn van Steenhoven <pip88nl@gmail.com>
2012-10-10 10:19:44 +0800

    syntax error fix

--- elkhound/elkhound/renumbering.ml
+++ elkhound/elkhound/renumbering.ml
@@ -129,7 +129,7 @@ let renumber_states env states =
     ) states;
   );
 
-  if Config.dump_automaton (
+  if Config.dump_automaton then (
     List.iter (PrintAnalysisEnv.print_item_set ~out:(Pervasives.open_out "automaton.out") env) states;
   );
 
