b6dbad92e5adf65467cc00944ef953da95b138a4
Wojciech Meyer <wojciech.meyer@gmail.com>
2012-10-10 23:00:29 +0100

    Move AST under elkhound directory

--- elkhound/ast/lit.ml
+++ elkhound/ast/lit.ml
@@ -2,9 +2,10 @@
                               Literals
   --------------------------------------------------------------------------*)
 
+type size = [ `I | `L | `LL | `U | `UL | `ULL ]
+
 type 'a t =
     Float of 'a * string
   | Int of 'a * size * string
   | String of 'a * string
   | Char of 'a * string
-and size = [| `I | `L | `LL | `U | `UL | `ULL |]
