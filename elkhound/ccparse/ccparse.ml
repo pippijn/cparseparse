70e08382d053fc723002afa6afa5b7cf4b91961c
Pippijn van Steenhoven <pip88nl@gmail.com>
2012-10-10 11:21:15 +0800

    support load/store of token lists

--- elkhound/ccparse/ccparse.ml
+++ elkhound/ccparse/ccparse.ml
@@ -6,17 +6,21 @@ module Options = struct
   let _utf8 = ref false
   let _pp = ref false
   let _tokens = ref false
+  let _dumptoks = ref false
+  let _loadtoks = ref false
   let _trivial = ref false
 end
 
 let () =
   Arg.(parse (align [
-    "-ptree",	Set Options._ptree,	       " build parse tree";
-    "-print",	Set Options._print,	       " print tree";
-    "-utf8",	Set Options._utf8,	       " assume source file is in UTF-8 encoding";
-    "-pp",	Set Options._pp,	       " fully tokenise before parsing";
-    "-tokens",	Set Options._tokens,	       " tokenise only; do not parse";
-    "-trivial",	Set Options._trivial,	       " use trivial user actions";
+    "-ptree",		Set Options._ptree,		" build parse tree";
+    "-print",		Set Options._print,		" print tree";
+    "-utf8",		Set Options._utf8,		" assume source file is in UTF-8 encoding";
+    "-pp",		Set Options._pp,		" fully tokenise before parsing";
+    "-tokens",		Set Options._tokens,		" tokenise only; do not parse";
+    "-dumptoks",	Set Options._dumptoks,		" dump tokens to file (implies -pp)";
+    "-loadtoks",	Set Options._loadtoks,		" load tokens from file";
+    "-trivial",		Set Options._trivial,		" use trivial user actions";
   ]) (fun input -> inputs := input :: !inputs) "Usage: cxxparse [option...] <file...>")
 
 
@@ -36,16 +40,8 @@ let handle_return = function
 
 
 let glrparse glr getToken =
-  let open Lexerint in
-
   let tree =
-    let lex = { tokType = 0; tokSval = SemanticValue.null } in
-
-    match Glr.glrParse glr getToken lex with
-    | Some tree ->
-        tree
-    | None ->
-        failwith "parse error"
+    Glr.glrParse glr getToken Lexerint.({ tokType = 0; tokSval = SemanticValue.null })
   in
 
   (* print accounting statistics from glr.ml *)
@@ -75,22 +71,56 @@ let rec tokenise tokens token lexbuf =
   )
 
 
-let parse glr actions cin lexer =
+let getToken_from_list tokens =
+  let tokens = ref tokens in
+  fun lex ->
+    let open Lexerint in
+    lex.tokType <- CcTokens.index (List.hd !tokens);
+    tokens := List.tl !tokens;
+    lex
+
+let getToken_from_lexer lexer lexbuf =
+  fun lex ->
+    let open Lexerint in
+    lex.tokType <- CcTokens.index (lexer.token lexbuf);
+    lex
+
+
+let getToken_from_dump input =
+  let tokens = Marshal.from_channel (open_in (input ^ ".tkd")) in
+  getToken_from_list tokens
+
+
+let getToken_from_file input lexer =
   let open Lexerint in
+
+  (* FIXME: this is never closed *)
+  let cin = Unix.open_process_in ("gcc -I /usr/include/qt4 -xc++ -E -P " ^ input) in
 
   let lexbuf = lexer.from_channel cin in
 
+  assert (not !Options._loadtoks);
+  if !Options._pp then (
+    let tokens = tokenise [] lexer.token lexbuf in
+    if !Options._dumptoks then (
+      if !Options._loadtoks then
+        failwith "-dumptoks and -loadtoks are mutually exclusive";
+      Marshal.to_channel (open_out (input ^ ".tkd")) tokens [Marshal.No_sharing];
+    );
+
+    getToken_from_list tokens
+  ) else (
+    assert (not !Options._dumptoks);
+    getToken_from_lexer lexer lexbuf
+  )
+
+
+let parse glr actions input lexer =
   let getToken =
-    if !Options._pp then
-      let tokens = ref (tokenise [] lexer.token lexbuf) in
-      fun lex ->
-        lex.tokType <- CcTokens.index (List.hd !tokens);
-        tokens := List.tl !tokens;
-        lex
+    if !Options._loadtoks then
+      getToken_from_dump input
     else
-      fun lex ->
-        lex.tokType <- CcTokens.index (lexer.token lexbuf);
-        lex
+      getToken_from_file input lexer
   in
 
   let getToken =
@@ -101,56 +131,38 @@ let parse glr actions cin lexer =
   in
 
   if !Options._tokens then
-    raise (ExitStatus 0);
+    None
+  else
+    glrparse glr getToken
 
-  glrparse glr getToken
 
-
-let parse_utf8 glr actions cin =
-  print_endline "utf8";
-  let open Lexerint in
-  parse glr actions cin {
+let parse_utf8 glr actions input =
+  parse glr actions input Lexerint.({
     from_channel = Ulexing.from_utf8_channel;
     lexeme = Ulexing.utf8_lexeme;
     lexeme_start = Ulexing.lexeme_start;
 
     token = Ulexer.token;
     line = Ulexer.line;
-  }
+  })
 
 
-let parse_ascii glr actions cin =
-  let open Lexerint in
-  parse glr actions cin {
+let parse_ascii glr actions input =
+  parse glr actions input Lexerint.({
     from_channel = Lexing.from_channel;
     lexeme = Lexing.lexeme;
     lexeme_start = Lexing.lexeme_start;
 
     token = Lexer.token;
     line = Lexer.line;
-  }
-
-
-let parse_channel glr actions cin =
-  if !Options._utf8 then
-    parse_utf8 glr actions cin
-  else
-    parse_ascii glr actions cin
+  })
 
 
 let parse_file glr actions input =
-  let cin = Unix.open_process_in ("gcc -I /usr/include/qt4 -xc++ -E -P " ^ input) in
-
-  let tree =
-    try
-      parse_channel glr actions cin
-    with
-    | e ->
-        handle_return (Unix.close_process_in cin);
-        raise e
-  in
-  handle_return (Unix.close_process_in cin);
-  tree
+  if !Options._utf8 then
+    parse_utf8 glr actions input
+  else
+    parse_ascii glr actions input
 
 
 let tokenKindDesc kind =
@@ -171,25 +183,30 @@ let elkmain inputs =
 
     let actions = PtreeActions.makeParseTreeActions actions tables in
     let trees = parse_files actions tables inputs in
-    List.iter (fun tree ->
-      if !Options._print then
-        PtreeNode.print_tree tree stdout true
+    List.iter (function
+      | None -> ()
+      | Some tree ->
+          if !Options._print then
+            PtreeNode.print_tree tree stdout true
     ) trees
 
   ) else if !Options._trivial then (
 
     let actions = UserActions.make_trivial actions in
-    List.iter (fun () -> ()) (parse_files actions tables inputs)
+    List.iter (function None | Some () -> ()) (parse_files actions tables inputs)
 
   ) else (
 
     (* unit list list *)
-    List.iter (List.iter (fun () -> ())) (parse_files actions tables inputs)
+    List.iter (function None -> () | Some l -> List.iter (fun () -> ()) l) (parse_files actions tables inputs)
 
   )
 
 
 let main inputs =
+  if !Options._dumptoks then
+    Options._pp := true;
+
   try
     elkmain inputs
   with ExitStatus status ->
