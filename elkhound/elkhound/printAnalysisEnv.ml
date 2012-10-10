c26d54b1691318439765cba906cdefa6a6c5037c
Pippijn van Steenhoven <pip88nl@gmail.com>
2012-10-10 10:12:02 +0800

    allow printing to a channel other than stdout

--- elkhound/elkhound/printAnalysisEnv.ml
+++ elkhound/elkhound/printAnalysisEnv.ml
@@ -1,22 +1,22 @@
-open Batteries_uni
 open GrammarType
 open AnalysisEnvType
 
 
-let print_terminal_set ?(abbreviate=false) ?(name=".") terms set =
+let print_terminal_set ?(out=stdout) ?(abbreviate=true) ?(name=".") terms set =
   let open GrammarType in
-  Printf.printf "[1;30mFirst(%s) = " name;
-  Printf.printf "%d {" (TerminalSet.cardinal set);
+  (*Printf.fprintf out "[1;30mFirst(%s) = " name;*)
+  Printf.fprintf out "First(%s) = " name;
+  Printf.fprintf out "%d {" (TerminalSet.cardinal set);
 
   if abbreviate then (
-    print_string " ..."
+    output_string out " ..."
   ) else (
     let first = ref true in
     Array.iter (fun term ->
       if TerminalSet.mem term.term_index set then (
         if not !first then
-          print_string ",";
-        print_string " ";
+          output_string out ",";
+        output_string out " ";
         first := false;
 
         let name =
@@ -25,72 +25,78 @@ let print_terminal_set ?(abbreviate=false) ?(name=".") terms set =
           else
             term.tbase.name
         in
-        print_string name
+        output_string out name
       )
     ) terms
   );
-  print_string " }";
-  print_string "[0m"
+  (*output_string out " }[0m"*)
+  output_string out " }"
 
 
-let print_dotted_production ?terms dprod =
-  print_string dprod.prod.left.nbase.name;
-  print_string " ->";
-  List.iteri (fun position rhs ->
-    print_string " ";
+let print_dotted_production ?(out=stdout) ?terms dprod =
+  output_string out dprod.prod.left.nbase.name;
+  output_string out " ->";
+  BatList.iteri (fun position rhs ->
+    output_string out " ";
     let after_dot =
       match dprod.after_dot with
       | None -> false
       | Some sym -> rhs == sym
     in
     if position = dprod.dot then
-      print_string ".[";
-    PrintGrammar.print_symbol rhs;
+      output_string out ".[";
+    PrintGrammar.print_symbol ~out rhs;
     if after_dot then
-      print_string "]";
+      output_string out "]";
   ) dprod.prod.right;
 
   if dprod.dot = List.length dprod.prod.right then
     (* dot is at end *)
-    print_string " .";
+    output_string out " .";
   
   match terms with
   | Some terms ->
-      print_string "  ";
-      print_terminal_set terms dprod.first_set
+      output_string out "  ";
+      print_terminal_set ~out terms dprod.first_set
   | None -> ()
 
 
-let print_lr_item env item =
-  print_dotted_production item.dprod;
-  print_string "  ";
-  print_terminal_set env.indexed_terms item.lookahead
+let print_lr_item ?(out=stdout) env item =
+  print_dotted_production ~out item.dprod;
+  output_string out "  ";
+  print_terminal_set ~out env.indexed_terms item.lookahead
 
 
-let print_item_set env item_set =
+let print_item_set ?(out=stdout) ?(print_nonkernels=false) env item_set =
   let print_lr_item item =
     (* print its text *)
-    print_string "  ";
-    print_lr_item env item;
-    print_string "\t\t";
+    output_string out "  ";
+    print_lr_item ~out env item;
+    output_string out "\t\t";
 
     (* print any transitions on its after-dot symbol *)
     begin match LrItem.symbol_after_dot item with
     | None -> () (* dot is at end *)
     | Some sym ->
         match ItemSet.transition item_set sym with
-        | None -> print_string "(no transition)"
-        | Some is -> Printf.printf "--> %d" (int_of_state_id is.state_id)
+        | None -> output_string out "(no transition)"
+        | Some is -> Printf.fprintf out "--> %d" (int_of_state_id is.state_id)
     end;
 
-    print_newline ();
+    output_string out "\n";
   in
 
-  Printf.printf "ItemSet %d {\n" (int_of_state_id item_set.state_id);
-  List.iter print_lr_item
-    (List.sort ~cmp:compare
-      (item_set.kernel_items.items @ item_set.nonkernel_items));
-  print_newline ();
+  Printf.fprintf out "ItemSet %d {\n" (int_of_state_id item_set.state_id);
+  if print_nonkernels then (
+    List.iter print_lr_item
+      (List.sort compare
+        (item_set.kernel_items.items @ item_set.nonkernel_items));
+  ) else (
+    List.iter print_lr_item
+      (List.sort compare
+        item_set.kernel_items.items);
+  );
+  output_string out "\n";
 
   (* print transition function directly, since I'm now throwing
    * away items sometimes *)
@@ -98,7 +104,7 @@ let print_item_set env item_set =
     match transition with
     | None -> ()
     | Some transition ->
-        Printf.printf "  on terminal %s go to %d\n"
+        Printf.fprintf out "  on terminal %s go to %d\n"
           env.indexed_terms.(i).tbase.name
           (int_of_state_id transition.state_id)
   ) item_set.term_transition;
@@ -107,15 +113,15 @@ let print_item_set env item_set =
     match transition with
     | None -> ()
     | Some transition ->
-        Printf.printf "  on nonterminal %s go to %d\n"
+        Printf.fprintf out "  on nonterminal %s go to %d\n"
           env.indexed_nonterms.(i).nbase.name
           (int_of_state_id transition.state_id)
   ) item_set.nonterm_transition;
 
   List.iter (fun item ->
-    print_string "  can reduce by ";
-    PrintGrammar.print_production item.dprod.prod;
-    print_newline ()
+    output_string out "  can reduce by ";
+    PrintGrammar.print_production ~out item.dprod.prod;
+    output_string out "\n";
   ) item_set.dots_at_end;
 
-  print_endline "}";
+  output_string out "}\n";
