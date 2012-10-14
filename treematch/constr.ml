f8a80750dfb3b6438871ed9306a47d1e54340427
Wojciech Meyer <wojciech.meyer@gmail.com>
2012-10-14 23:04:27 +0100

    Constructor printer

--- treematch/constr.ml
+++ treematch/constr.ml
@@ -7,3 +7,12 @@ open Sexplib.Conv
 type t = constr
 and constr = string * string list
 with sexp
+
+open ExtFormat
+
+let f = fprintf
+
+class print = object (self : 'a)
+  method constr pp (nm, l) =
+    f pp "%s@ %a" nm (pp_list pp_print_string pp_space_sep) l
+end
