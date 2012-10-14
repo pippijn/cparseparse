f3b511fbd5ea0c1ca94cb930a9869bcdeaee1bec
Wojciech Meyer <wojciech.meyer@gmail.com>
2012-10-14 23:49:47 +0100

    Add printer for trees

--- treematch/tree.ml
+++ treematch/tree.ml
@@ -13,3 +13,16 @@ and const =
     String of string
   | Int of int
 with sexp
+
+open ExtFormat
+let f = fprintf
+
+class print = object (self : 'a)
+  method tree pp = function
+  | Tree (nm, lst) -> f pp "@[<hov>(%s@ @[<hov 2>%a@])@]" nm (pp_list self # tree pp_space_sep) lst
+  | Var nm -> pp_print_string pp nm
+  | Const c -> f pp "%a" self#const c
+  method const pp = function
+  | Int i -> f pp "%d" i
+  | String s -> f pp "\"%s\"" s
+end
