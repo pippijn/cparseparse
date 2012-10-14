c0a07a89e58c41b6cf9eacf8824422e4396d7a7a
Wojciech Meyer <wojciech.meyer@gmail.com>
2012-10-14 15:35:17 +0100

    Dump tokens in options

--- treematch/options.ml
+++ treematch/options.ml
@@ -2,6 +2,7 @@ module Priv = struct
   let _i = ref false
   let _infer = ref false
   let _dump_ast = ref false
+  let _dump_tokens = ref false
   let inputs = ref []
 end
 
@@ -10,10 +11,12 @@ let () =
     "-i",                       Set Priv._i,		" generate interface files";
     "-infer",                   Set Priv._infer,        " infer the ast from map strategies";
     "-dump-ast",		Set Priv._dump_ast,	" dump source ast";
+    "-dump-tokens",		Set Priv._dump_ast,	" dump source tokens";
   ]) (fun input -> Priv.inputs := input :: !Priv.inputs) "Usage: treematch [option...] <file...>")
 
 let _i = !Priv._i
 let _infer = !Priv._infer
 let _dump_ast = !Priv._dump_ast
+let _dump_tokens =  !Priv._dump_tokens
 
 let inputs = List.rev !Priv.inputs
