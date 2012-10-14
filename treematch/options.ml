f05ffc46e97ba0214ee905e0032a0106fc03afa7
Wojciech Meyer <wojciech.meyer@gmail.com>
2012-10-14 18:54:32 +0100

    -no-emit option

--- treematch/options.ml
+++ treematch/options.ml
@@ -1,4 +1,5 @@
 module Priv = struct
+  let _no_emit = ref false
   let _i = ref false
   let _infer = ref false
   let _dump_ast = ref false
@@ -8,12 +9,14 @@ end
 
 let () =
   Arg.(parse (align [
+    "-no-emit",                 Set Priv._i,		" Don't generate code";
     "-i",                       Set Priv._i,		" generate interface files";
     "-infer",                   Set Priv._infer,        " infer the ast from map strategies";
     "-dump-ast",		Set Priv._dump_ast,	" dump source ast";
     "-dump-tokens",		Set Priv._dump_tokens,	" dump source tokens";
   ]) (fun input -> Priv.inputs := input :: !Priv.inputs) "Usage: treematch [option...] <file...>")
 
+let _no_emit = !Priv._no_emit
 let _i = !Priv._i
 let _infer = !Priv._infer
 let _dump_ast = !Priv._dump_ast
