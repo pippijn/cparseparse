d1e06dc480aa966800b4f91e93ade259a4bd1724
Wojciech Meyer <wojciech.meyer@gmail.com>
2012-10-14 23:50:08 +0100

    Map now will be pretty printed

--- treematch/program.ml
+++ treematch/program.ml
@@ -21,22 +21,31 @@ and topl_tree = Constr.t
 with sexp
 
 open ExtFormat
-
 let f = fprintf
+
+let pp_biarrow_sep pp () = f pp "@ =>@ "
 
 class print = object (self : 'a)
   method program pp = f pp "@[<v>%a@]@;@." (pp_list self # definition pp_space_sep)
   method definition pp x =
     match x with
-    Ast (nm, nodes) -> f pp "@[<v>@[<h>ast@ %s@]@;{@[<v 2>@;@[<v>%a@]@]@;}@]@;" nm (pp_list self # ast_node pp_space_sep) nodes
-  | Map (nm, types, nodes) -> ()
+    Ast (nm, nodes) ->
+      f pp "@[<v>@[<h>ast@ %s@]@;{@[<v 2>@;@[<v>%a@]@]@;}@]@;"
+        nm (pp_list self # ast_node pp_space_sep) nodes
+  | Map (nm, types, nodes) ->
+      f pp "@[<v>@[<h>map@ %s@ %a@]@;{@[<v 2>@;@[<v>%a@]@]@;}@]@;"
+        nm self # type_decl types
+        (pp_list self # rewrite_node pp_space_sep) nodes
   method ast_node pp (nm, node) = f pp "@[<hov>%s:@ %a@]" nm self # node node
-  method rewrite_node () ((nm, clauses) : (unit * unit))  = ()
-  method rewrite_clause () ((ltree, rtree) : (unit * unit)) = ()
+  method rewrite_node pp (nm, clauses) =
+    f pp "@[<hov>%s:@ %a@]" nm (pp_list self # rewrite_clause pp_newline_bar_sep) clauses
+  method rewrite_clause pp (ltree, rtree) =
+    f pp "@[<hov 2>%a@ =>@ %a@]" (new Tree.print) # tree ltree (new Tree.print) # tree rtree
   method node pp = function
     CustomNode clauses -> f pp "%a" (pp_list self # ast_clause pp_newline_bar_sep) clauses
   | NativeNode name -> pp_print_string pp name
-  method type_decl () (lst : unit) = ()
+  method type_decl pp lst =
+    f pp "@[<hov 2>%a@]" (pp_list pp_print_string pp_biarrow_sep) lst
   method ast_clause = self # topl_tree
   method topl_tree = (new Constr.print) # constr
 end
