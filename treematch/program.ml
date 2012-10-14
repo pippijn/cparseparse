04fa8ba765f1caeb49dfa7b658cd4496a69a1cbd
Wojciech Meyer <wojciech.meyer@gmail.com>
2012-10-14 19:51:50 +0100

    Add rudimentary Map definition ast

--- treematch/program.ml
+++ treematch/program.ml
@@ -6,11 +6,16 @@ open Sexplib.Conv
 
 type t = program
 and program = definition list
-and definition = Ast of string * ast_node list
+and definition =
+    Ast of string * ast_node list
+  | Map of string * type_decl * rewrite_node list
 and ast_node = string * node
+and rewrite_node = rewrite_clause list
+and rewrite_clause = Tree.t * Tree.t
 and node =
     CustomNode of ast_clause list
   | NativeNode of string
+and type_decl = string list
 and ast_clause = topl_tree
 and topl_tree = Constr.t
 with sexp
