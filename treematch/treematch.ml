9af9919f67ffd35920be2022827267ab311783b4
Wojciech Meyer <wojciech.meyer@gmail.com>
2012-10-17 03:11:39 +0100

    Print typed program

--- treematch/treematch.ml
+++ treematch/treematch.ml
@@ -57,6 +57,7 @@ let files =
       program |> p # program Format.std_formatter;
       Typing.Collect.print Format.std_formatter "ListB" program;
       program |> Typing.program |> Program.output_typed_program stdout;
+      program |> Typing.program |> (new Program.typed_print)#program Format.std_formatter;
       ()
     )
   in
