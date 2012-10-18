6f26245646a22e74d7cab36982d2055b0b3d9dbb
Wojciech Meyer <wojciech.meyer@gmail.com>
2012-10-18 06:01:22 +0100

    Pretty print correctly Tycons

--- src/treematch/tree.ml
+++ src/treematch/tree.ml
@@ -17,6 +17,12 @@ with sexp
 open ExtFormat
 let f = fprintf
 
+let type_of = function
+| Tree (ty, _) -> ty
+| Var (ty,_) -> ty
+| Const (String _) -> Constr.Tycon (Ident.uident "string")
+| Const (Int _) -> Constr.Tycon (Ident.uident "string")
+
 class print = object (self : 'a)
   method tree pp = function
   | Tree ((), (nm, lst)) -> f pp "@[<hov>(%a@ @[<hov 2>%a@])@]" Ident.pp_uident nm (pp_list self # tree pp_space_sep) lst
@@ -29,8 +35,8 @@ end
 
 class typed_print = object (self : 'a)
   method tree pp = function
-  | Tree (ty, (nm, lst)) -> f pp "@[<hov>((%a@ @[<hov 2>%a@])@ : @ %a)@]" Ident.pp_uident nm (pp_list self # tree pp_space_sep) lst Ident.pp_uident ty
-  | Var (ty,nm) -> f pp "@[<hov>(%a@ : @ %a)@]" Ident.pp_lident nm Ident.pp_uident ty
+  | Tree (ty, (nm, lst)) -> f pp "@[<hov>((%a@ @[<hov 2>%a@])@ : @ %a)@]" Ident.pp_uident nm (pp_list self # tree pp_space_sep) lst (new Constr.print) # tycon ty
+  | Var (ty,nm) -> f pp "@[<hov>(%a@ : @ %a)@]" Ident.pp_lident nm (new Constr.print) # tycon ty
   | Const c -> f pp "%a" self#const c
   method const pp = function
   | Int i -> f pp "%d" i
