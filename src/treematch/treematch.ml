2e4534e44951005fff307b763061cb1314f66fc8
Wojciech Meyer <wojciech.meyer@gmail.com>
2012-10-18 06:01:22 +0100

    some rework of the driver

--- src/treematch/treematch.ml
+++ src/treematch/treematch.ml
@@ -37,37 +37,36 @@ let rec tokenise tokens token lexbuf =
 
 let files =
   let single name =
-    let parse () =
+    let program =
       let lexbuf = Lexing.from_channel (open_in name) in
       Parser.program Lexer.token lexbuf in
 
     if Options._dump_tokens () then (
       Printf.printf "***** Processing %s\n" name;
+      begin
+        let lexbuf = Lexing.from_channel (open_in name) in
+        let tokens = tokenise [] Lexer.token lexbuf in
+        List.iter (print_endline -| Token.to_string) tokens;
+      end;
+      Printf.printf "***** End of %s\n" name);
 
-      let lexbuf = Lexing.from_channel (open_in name) in
-      let tokens = tokenise [] Lexer.token lexbuf in
-      List.iter (print_endline -| Token.to_string) tokens;
-
-      Printf.printf "***** End of %s\n" name;
-
-      flush stdout);
 
     if Options._dump_ast () then (
-      parse () |> Program.output_untyped_program stdout
+      program |> Program.output_untyped_program stdout
     );
 
     if not (Options._no_emit ()) then (
-      parse () |> SimpleBackend.output_program "/dev/stdout"
+      program
+      |> if Options._special ()
+         then SimpleBackend.output_program "/dev/stdout"
+         else assert false
     );
 
     if Options._infer () then (
-
-      let program = parse () in
-
       List.iter ((|>) program) [
         (new Program.print) # program Format.std_formatter;
         Typing.program |- Program.output_typed_program stdout;
-        Typing.program |- (new Program.typed_print)#program Format.std_formatter
+        Typing.program |- (new Program.typed_print)#program Format.std_formatter;
       ]
 
     )
