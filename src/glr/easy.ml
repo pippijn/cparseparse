7b134276ed2fae6acc9b79a005c028ed2621753c
Pippijn van Steenhoven <pip88nl@gmail.com>
2012-10-20 14:52:48 +0800

    added "easy" module for ocamllex lexers

---
+++ src/glr/easy.ml
@@ -0,0 +1,68 @@
+module type Config = sig
+  (* make it print a parse tree instead of evaluating the expression *)
+  val ptree : bool
+  (* produce a typed parse tree and print it as s-expressions *)
+  val typed_ptree : bool
+end
+
+
+module Make
+  (Actions : UserActions.S)
+  (Tables : ParseTablesType.S)
+  (Ptree : PtreeActions.S)
+  (PtreeAct : UserActions.S with type result = Ptree.t)
+  (Tokens : Lexerint.TokenInfo)
+  (Config : Config)
+= struct
+
+  let parse actions tables lexer =
+    let glr = GlrEngine.makeGLR actions tables in
+
+    GlrEngine.glrParse glr lexer
+
+
+  let parse token lexbuf action =
+    let tables = Tables.parseTables in
+    let actions = Actions.userActions in
+
+    let lexer = Lexerint.({
+      token = (fun () -> token lexbuf);
+      index = Tokens.index;
+      sval  = Tokens.sval;
+      sloc  = (fun _ -> Lexing.dummy_pos, Lexing.dummy_pos);
+    }) in
+
+    if Config.ptree then (
+      
+      let actions = PtreeActions.make_actions actions tables in
+      let lexer = PtreeActions.make_lexer actions lexer in
+
+      match parse actions tables lexer with
+      | None      ->
+          print_endline "parsing failed"
+      | Some tree ->
+          PtreeNode.print_tree tree stdout true
+
+    ) else if Config.typed_ptree then (
+      
+      let actions = PtreeAct.userActions in
+
+      match parse actions tables lexer with
+      | None      ->
+          print_endline "parsing failed"
+      | Some tree ->
+          Sexplib.Sexp.output_hum stdout
+            (Ptree.sexp_of_t tree);
+          print_newline ()
+
+    ) else (
+
+      match parse actions tables lexer with
+      | None      ->
+          print_endline "parsing failed"
+      | Some result ->
+          action result
+
+    )
+
+end
