bdc28a3557e0128b995fd89c09be8411c9f30a24
Pippijn van Steenhoven <pip88nl@gmail.com>
2012-10-21 17:14:46 +0800

    comment on design

--- src/baselib/integralIndexedArray.mli
+++ src/baselib/integralIndexedArray.mli
@@ -1,3 +1,10 @@
+(* This type is hidden. The type inside the functor is visible and defined in
+ * terms of this hidden type, so we can define common operations on all
+ * instances of the Make functor.
+ *
+ * All common operations drop the phantom types. If there is a common
+ * operation that does not drop the phantom type, it should be defined inside
+ * the functor. *)
 type ('a, 'b) repr
 
 val append : ('a, 'b) repr -> ('a, 'c) repr -> 'a array
