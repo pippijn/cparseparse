ff73aa4ea66debc39bd9e06fb3a572101eecf95a
Pippijn van Steenhoven <pip88nl@gmail.com>
2012-10-21 14:26:53 +0800

    added treematch ast backend to gramanl

---
+++ src/gramanl/emitTreematch.ml
@@ -0,0 +1,155 @@
+open Camlp4.PreCast
+open GrammarType
+open CodegenHelpers
+
+module OCamlPrinter = Camlp4.Printers.OCaml.Make(Syntax)
+
+let (|>) = BatPervasives.(|>)
+let _loc = Loc.ghost
+
+
+(************************************************
+ * :: Concrete syntax tree
+ ************************************************)
+
+let string_of_ctyp ctyp =
+  let printer = new OCamlPrinter.printer () in
+  printer#ctyp Format.str_formatter ctyp;
+  Format.flush_str_formatter ()
+
+
+let ctyp_of_nonterminal nonterm =
+  (* the type is the referenced nonterminal module *)
+  let typ = nonterm.nbase.name in
+  assert (is_uid typ);
+  typ
+
+
+let ctyp_of_terminal term =
+  (* use the terminal type *)
+  match term.tbase.semtype with
+  | None ->
+      failwith "tagged terminals must have a declared type"
+  | Some ty ->
+      term.tbase.name
+
+
+let ctyp_of_symbol = function
+  | Nonterminal (_, nonterm) -> ctyp_of_nonterminal nonterm
+  | Terminal    (_,    term) -> ctyp_of_terminal term
+
+
+let ctyp_of_right_symbol head_tail =
+  PtreeMaker.symbols_of_production head_tail
+  |> List.rev
+  |> List.hd
+  |> ctyp_of_symbol
+
+
+(* XXX: if this function changes its output, PtreeMaker.prods probably
+ * also needs to change *)
+let production_types term_mods left has_merge prods =
+  match prods with
+  (* TODO: figure out how to do this with treematch
+  (* nonterminal with a single production that is a tagged symbol
+   * (and there is no merge) *)
+  | [{ right = [Terminal (tag, _) | Nonterminal (tag, _) as sym] }] when tag <> "" && not has_merge ->
+      let semtype = ctyp_of_symbol sym in
+      left ^ ": " ^ semtype
+
+  | [tail; head_tail] when PtreeMaker.is_list_nonterminal tail head_tail && not has_merge ->
+      let semtype = ctyp_of_right_symbol head_tail in
+      left ^ ": " ^ semtype ^ " list"
+
+  | [none; some] when PtreeMaker.is_option_nonterminal none some && not has_merge ->
+      let semtype = ctyp_of_right_symbol some in
+      left ^ ": " ^ semtype ^ " option"
+
+  | [none; some] when PtreeMaker.is_boolean_nonterminal none some && not has_merge ->
+      left ^ ": bool"
+  *)
+
+  | prods ->
+      let types =
+        List.map (fun prod ->
+          let prod_type =
+            List.map (fun sym ->
+              match sym with
+              | Nonterminal ("", _)
+              | Terminal ("", _) ->
+                  (* nothing to do for untagged symbols *)
+                  []
+
+              | Nonterminal (_, nonterm) ->
+                  [ctyp_of_nonterminal nonterm]
+              | Terminal (_, ({ tbase = { name; semtype = Some semtype; } } as term)) ->
+                  if not (Hashtbl.mem term_mods name) then
+                    Hashtbl.add term_mods name (string_of_ctyp semtype);
+                  [ctyp_of_terminal term]
+
+              | _ -> failwith "bad"
+
+            ) prod.right
+            |> List.concat
+            |> String.concat " "
+          in
+
+          let prod_name =
+            match prod.prod_name with
+            | None      -> "P" ^ StateId.Production.to_string prod.prod_index
+            | Some name -> assert (is_uid name); name
+          in
+
+          prod_name ^ " " ^ prod_type
+        ) prods
+      in
+
+      let types =
+        if has_merge then
+          ("Merge " ^ left ^ " " ^ left) :: types
+        else
+          types
+      in
+
+      let types = String.concat "\n\t| " types in
+
+      left ^ ": " ^ types
+
+
+
+let make_ml_treematch prods prods_by_lhs =
+  let term_mods = Hashtbl.create 13 in
+
+  let bindings =
+    List.rev (Array.fold_left (fun bindings indices ->
+      match List.map (ProdArray.get prods) indices with
+      | [] ->
+          (* the empty nonterminal has no productions *)
+          bindings
+
+      | { left = { nbase = { name } } } :: _ when name.[0] = '_' ->
+          (* we do not emit code for the synthesised start rule *)
+          bindings
+
+      | first :: _ as prods ->
+          let nonterm = first.left in
+          let name = nonterm.nbase.name in
+          assert (is_uid name);
+
+          let has_merge = nonterm.merge != None in
+
+          let types = production_types term_mods name has_merge prods in
+
+          types :: bindings
+    ) [] prods_by_lhs)
+  in
+
+  let bindings =
+    Hashtbl.fold (fun name typ bindings ->
+      (name ^ ": " ^ typ) :: bindings
+    ) term_mods bindings
+  in
+
+  "ast Ptree {\n"
+  ^ String.concat "\n" bindings
+  ^ "\n}\n"
