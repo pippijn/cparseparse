99071be87137c05077efa5960e8c2c19379a3fa7
Pippijn van Steenhoven <pip88nl@gmail.com>
2012-10-27 19:15:38 +0800

    added character class utilities

---
+++ src/re2ml/charClass.ml
@@ -0,0 +1,69 @@
+open Ast
+
+let of_array =
+  Array.fold_left (fun l -> function
+    | None -> l
+    | Some c -> c :: l
+  ) []
+
+
+let compress list =
+  let chars = Array.make 256 None in
+  List.iter (fun c ->
+    chars.(Char.code (Sloc.value c)) <- Some c
+  ) list;
+  of_array chars
+
+
+let rec make_range range low high =
+  if low > high then
+    range
+  else
+    make_range (Char.chr low :: range) (low + 1) high
+
+
+let chrs_of_range = function
+  (* single characters need no resolution *)
+  | Single c -> [c]
+  (* resolve ranges as list of characters in the range *)
+  | Range (low, high) ->
+      let range =
+        make_range []
+          (Char.code (Sloc.value low))
+          (Char.code (Sloc.value high))
+      in
+      List.rev_map (Sloc.at low) range
+
+
+let chrs_of_ranges list =
+  compress (List.concat (List.map chrs_of_range list))
+
+
+let full_chr_list = chrs_of_range (Range (Sloc.generated '\000', Sloc.generated '\255'))
+let empty_chr_list = []
+
+
+let difference (a : char Sloc.t list) (b : char Sloc.t list) =
+  let chars = Array.make 256 None in
+  List.iter (fun c ->
+    chars.(Char.code (Sloc.value c)) <- Some c
+  ) a;
+  List.iter (fun c ->
+    chars.(Char.code (Sloc.value c)) <- None
+  ) b;
+  of_array chars
+
+
+let invert cc =
+  difference full_chr_list cc
+
+
+let to_chr_list = function
+  (* resolve positive character classes as list of its characters *)
+  | Positive list ->
+      chrs_of_ranges list
+
+  (* resolve Negative character classes as the difference between
+   * the full range and this class *)
+  | Negative list ->
+      invert (chrs_of_ranges list)
